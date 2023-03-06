cfc31ba4357012f4152ff3f3ddad3704d3ed19cc
KINastya <kilusiknastya@gmail.com>
2023-03-06 17:24:41 +0200

    Did my homework

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const number = "73733";
+console.log(number.split('').join(" "));
+
+const line1 = prompt("Please write your firs line");
+const line2 = prompt("Please write your second line");
+const line3 = prompt("Please write your third line");
+console.log(`${line1} \n ${line2} \n ${line3}`);
